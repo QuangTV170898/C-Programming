11d84f0ac92853a63e310a461ae9a78e1e140bfa
QuangTV98 <tranvietquang2016@gmail.com>
2023-03-03 09:27:30 +0700

    structural theory stack

---
+++ stack/stack.c
@@ -0,0 +1,15 @@
+/**
+ * LIFO 
+ * push(): them 1 element vao trong stack.
+ * pop(): lay 1 element ra khoi stack va xoa n di.
+ * top(): lay 1 element de xem gia tri cua n chu khong xoa n ra khoi stack.
+ * size():
+ * isStackEmpty():
+ * isStackFull():
+ * stack empty hay stack da day.
+ * stack implementations
+ * - Simple Array: Khong the thay doi size
+ * - Dynamic Array: Co the thay doi size bang cach tao ra array moi va copy 
+ * - Linked list
+ * 
+ */
