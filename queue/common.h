accdb0797841f2ff4c4ec405dd4eab1c3e62d907
QuangTV98 <tranvietquang2016@gmail.com>
2023-03-03 09:34:31 +0700

    structural theory queue

---
+++ queue/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <pthread.h>
+
+typedef pthread_mutex_t tMutex;
+typedef pthread_cond_t tCond;
+
+/* mutex function */
+/* condition function */
+
+#endif
---
+++ queue/queue.c
@@ -0,0 +1,16 @@
+/**
+ * Vao truoc thi ra truoc FIFO. 
+ * Hang doi giong nhu nguoi xep hang de mua hang.
+ * Vd: Xep hang mua cf thi ai xep truoc thi duoc mua truoc.
+ * Di vao hang thi di tu phia sau cua hang.
+ * Di ra hang thi di tu phia truoc cua hang.
+ * Enqueue(): cho 1 element gia nhap vao dang sau cua hang doi.
+ * Dequeue(): 1 element duoc lay ra khoi hang doi.
+ * Front(): lay gia tri cua hang doi nhung khong lay n ra khoi hang doi.
+ * Size(): so luong element trong Queue.
+ * IsEmptyQueue(): Queue co rong hay khong.
+ * Queue Implementations
+ *  - Simple Circular Array Based Queue
+    - Dynamic Circular Array Based Queue
+    - Linked List Queue
+ */
